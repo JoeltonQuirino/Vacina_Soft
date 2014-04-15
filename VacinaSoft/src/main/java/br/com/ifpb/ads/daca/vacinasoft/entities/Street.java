91cbd82b891e7d0ae35e072b1fe73ea99588e48e
WitaloCarlos <witalocarlos@gmail.com>
2014-04-15 20:48:36 -0300

    Adição das Entidades PSFUser, Area, Street e HealthAgent; Adição da Entidade Abstrata

---
+++ VacinaSoft/src/main/java/br/com/ifpb/ads/daca/vacinasoft/entities/Street.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.ifpb.ads.daca.vacinasoft.entities;
+
+/**
+ *
+ * @author WitaloCarlos
+ */
+public class Street {
+    
+    private String district;
+    private String nameStreet;
+    private int number;
+    private String complement;
+    private String state;
+    private String postalCode;
+    private String countryCode;
+    
+    
+    
+    
+}
