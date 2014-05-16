683ea397cae56b2119b14a9b78ef7a47cbd3539e
WitaloCarlos <witalocarlos@gmail.com>
2014-05-15 23:06:58 -0300

    Correção dos DAOs, adição das páginas JSF e classes controller geradas pelo NetBeans

---
+++ VacinaSoft/src/main/java/br/com/ifpb/ads/daca/vacinasoft/dao/session/AdministrationFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.ifpb.ads.daca.vacinasoft.dao.session;
+
+import br.com.ifpb.ads.daca.vacinasoft.entities.Administration;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author WitaloCarlos
+ */
+@Stateless
+public class AdministrationFacade extends AbstractFacade<Administration> {
+    @PersistenceContext(unitName = "br.com.ifpb.ads.daca_VacinaSoft_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AdministrationFacade() {
+        super(Administration.class);
+    }
+    
+}
