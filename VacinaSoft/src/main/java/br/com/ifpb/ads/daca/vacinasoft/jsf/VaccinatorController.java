a72193d4d395c527cc43893d43ab5540da8a3032
WitaloCarlos <witalocarlos@gmail.com>
2014-05-16 21:21:08 -0300

    Correção dos DAOs, adição das páginas JSF e classes controller geradas pelo NetBeans

---
+++ VacinaSoft/src/main/java/br/com/ifpb/ads/daca/vacinasoft/jsf/VaccinatorController.java
@@ -0,0 +1,165 @@
+package br.com.ifpb.ads.daca.vacinasoft.jsf;
+
+import br.com.ifpb.ads.daca.vacinasoft.entities.Vaccinator;
+import br.com.ifpb.ads.daca.vacinasoft.jsf.util.JsfUtil;
+import br.com.ifpb.ads.daca.vacinasoft.jsf.util.JsfUtil.PersistAction;
+import br.com.ifpb.ads.daca.vacinasoft.dao.session.VaccinatorFacade;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@Named("vaccinatorController")
+@SessionScoped
+public class VaccinatorController implements Serializable {
+
+    @EJB
+    private br.com.ifpb.ads.daca.vacinasoft.dao.session.VaccinatorFacade ejbFacade;
+    private List<Vaccinator> items = null;
+    private Vaccinator selected;
+
+    public VaccinatorController() {
+    }
+
+    public Vaccinator getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Vaccinator selected) {
+        this.selected = selected;
+    }
+
+    protected void setEmbeddableKeys() {
+    }
+
+    protected void initializeEmbeddableKey() {
+    }
+
+    private VaccinatorFacade getFacade() {
+        return ejbFacade;
+    }
+
+    public Vaccinator prepareCreate() {
+        selected = new Vaccinator();
+        initializeEmbeddableKey();
+        return selected;
+    }
+
+    public void create() {
+        persist(PersistAction.CREATE, ResourceBundle.getBundle("/resources/Bundle").getString("VaccinatorCreated"));
+        if (!JsfUtil.isValidationFailed()) {
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public void update() {
+        persist(PersistAction.UPDATE, ResourceBundle.getBundle("/resources/Bundle").getString("VaccinatorUpdated"));
+    }
+
+    public void destroy() {
+        persist(PersistAction.DELETE, ResourceBundle.getBundle("/resources/Bundle").getString("VaccinatorDeleted"));
+        if (!JsfUtil.isValidationFailed()) {
+            selected = null; // Remove selection
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public List<Vaccinator> getItems() {
+        if (items == null) {
+            items = getFacade().findAll();
+        }
+        return items;
+    }
+
+    private void persist(PersistAction persistAction, String successMessage) {
+        if (selected != null) {
+            setEmbeddableKeys();
+            try {
+                if (persistAction != PersistAction.DELETE) {
+                    getFacade().edit(selected);
+                } else {
+                    getFacade().remove(selected);
+                }
+                JsfUtil.addSuccessMessage(successMessage);
+            } catch (EJBException ex) {
+                String msg = "";
+                Throwable cause = ex.getCause();
+                if (cause != null) {
+                    msg = cause.getLocalizedMessage();
+                }
+                if (msg.length() > 0) {
+                    JsfUtil.addErrorMessage(msg);
+                } else {
+                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/resources/Bundle").getString("PersistenceErrorOccured"));
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/resources/Bundle").getString("PersistenceErrorOccured"));
+            }
+        }
+    }
+
+    public Vaccinator getVaccinator(long id) {
+        return getFacade().find(id);
+    }
+
+    public List<Vaccinator> getItemsAvailableSelectMany() {
+        return getFacade().findAll();
+    }
+
+    public List<Vaccinator> getItemsAvailableSelectOne() {
+        return getFacade().findAll();
+    }
+
+    @FacesConverter(forClass = Vaccinator.class)
+    public static class VaccinatorControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            VaccinatorController controller = (VaccinatorController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "vaccinatorController");
+            return controller.getVaccinator(getKey(value));
+        }
+
+        long getKey(String value) {
+            long key;
+            key = Long.parseLong(value);
+            return key;
+        }
+
+        String getStringKey(long value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof Vaccinator) {
+                Vaccinator o = (Vaccinator) object;
+                return getStringKey(o.getId());
+            } else {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), Vaccinator.class.getName()});
+                return null;
+            }
+        }
+
+    }
+
+}
