c824d8e5cd86dfafb00bdefb68f194bc8fec0e7a
MarkusPatriota <MarkusPatriota@ASUS-PC>
2014-09-28 23:16:18 -0300

    Creation and implementation of service, changes in GUI and creating the graphics folder.

--- VacinaSoft/src/main/java/br/com/ifpb/ads/vacinasoft/services/StreetService.java
+++ VacinaSoft/src/main/java/br/com/ifpb/ads/vacinasoft/services/StreetService.java
@@ -19,7 +19,7 @@ public class StreetService extends AbstractService<Street> implements StreetServ
     
     @Override
     public GenericDaoInterface<Street> getDaoInterface() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return streetDaoInterface;
     }
     
 }
